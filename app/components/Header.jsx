2b0ba99305561b62747a0e43b810ddeb80d397a5
Massika-Ke <wamwoka99@gmail.com>
2025-01-06 23:52:06 +0300

    Initial Commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,24 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+        </div>
+        <h3 className='flex items-end gap-2 text-xl  md:text-2xl mb-3 font-Ovo'>
+            Hi! I'm Joseph Masika <Image src={assets.hand_icon} alt='' className='w-6'/>
+        </h3>
+        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+            Software and Network engineer based in Kenya.
+        </h1>
+        <p className='max-w-2xl mx-auto font-Ovo'>
+            I am a full stack web developer and network engineer from Nairobi, Kenya with 3 years of experience in multiple companies like Konnect and Fidelity International Hospital.
+        </p>
+    </div>
+  )
+}
+
+export default Header
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
         sideMenuRef.current.style.transform = 'translateX(-16rem'
     }
     const closeMenu =()=>{
-        sideMenuRef.current.style.transform = 'transalteY(16rem)'
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
     }
-    
+
   return (
     <>
         <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
@@ -41,7 +41,7 @@ const Navbar = () => {
                     Contact 
                     <Image src={assets.arrow_icon} alt="" className='w-3' /></a>
 
-                    <button className='block md:hidden ml-3'>
+                    <button className='block md:hidden ml-3' onClick={openMenu}>
                     <Image src={assets.menu_black} alt='' className='w-6'/> 
                 </button>
             </div>
@@ -51,14 +51,14 @@ const Navbar = () => {
             <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64
              top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 '>
 
-                <div className='absolute right-6 top-6'>
+                <div className='absolute right-6 top-6' onClick={closeMenu}>
                     <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
                 </div>
-                <li><a className='font-Ovo' href='#top'>Home</a></li>
-                <li><a className='font-Ovo' href='#about'>About Me</a></li>
-                <li><a className='font-Ovo' href='#services'>Services</a></li>
-                <li><a className='font-Ovo' href='#works'>Works</a></li>
-                <li><a className='font-Ovo' href='#contact'>Contact Me</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href='#top'>Home</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href='#about'>About Me</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href='#services'>Services</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href='#works'>Works</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href='#contact'>Contact Me</a></li>
             </ul>
         </nav>
     </>
