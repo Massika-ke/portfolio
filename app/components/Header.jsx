f48777825a202fce75002e3a88f6841f8db80e37
Massika-Ke <wamwoka99@gmail.com>
2025-01-07 00:39:37 +0300

    Feature Update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -17,8 +17,16 @@ const Header = () => {
         <p className='max-w-2xl mx-auto font-Ovo'>
             I am a full stack web developer and network engineer from Nairobi, Kenya with 3 years of experience in multiple companies like Konnect and Fidelity International Hospital.
         </p>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+            <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+                contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/>
+            </a>
+            <a href='/joseph-resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+                my resume <Image src={assets.download_icon} alt='' className='w-4'/>
+            </a>
+        </div>
     </div>
   )
 }
 
-export default Header
+export default Header;
