238912c12213d034d6418b73569ee315adc51913
Massika-Ke <wamwoka99@gmail.com>
2025-01-11 23:03:46 +0300

    Initial Commit

---
+++ app/components/Work.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div id='work'  className="w-full px-[12%] py-10 scroll-mt-20">
+        <h4 className="text-center mb-2 text-lg font-Ovo">My Portfolio</h4>
+        <h2 className="text-center text-5xl font-Ovo">My latest works</h2>
+
+        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+        I am a full stack web developer and network engineer from Nairobi, 
+        Kenya with 3 years of experience in multiple companies like Konnect and Fidelity International Hospital.
+        </p>
+    </div>
+  )
+}
+
+export default Work;
