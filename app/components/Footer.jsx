ecd5231409a5a8b7c856b76e75d32e94f54dbc15
Massika-Ke <wamwoka99@gmail.com>
2025-01-14 12:32:16 +0300

    Initial Commit

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,14 +2,14 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({isDarkMode}) => {
   return (
     <div className='mt-20'>
         <div className='text-center'>
             <Image src={assets.logo} alt='' className='w-28 cursor-pointer mx-auto mb-2 rounded-full' />
 
             <div className='w-max flex items-center gap-2 mx-auto'>
-                <Image src={assets.mail_icon} alt='' className='w-6' />
+                <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6' />
                 wamwoka99@gmail.com
             </div>
         </div>
