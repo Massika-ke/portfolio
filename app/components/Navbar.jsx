9cd64b2cbdfe26068402677ebb0059505bd233b3
Massika-Ke <wamwoka99@gmail.com>
2025-01-14 12:28:32 +0300

    Initial Commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             </a>
 
             <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
-                ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"} `}>
+                ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"} `}>
                 <li><a className='font-Ovo' href='#top'>Home</a></li>
                 <li><a className='font-Ovo' href='#about'>About Me</a></li>
                 <li><a className='font-Ovo' href='#services'>Services</a></li>
@@ -50,22 +50,23 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
                     <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6'/> 
                 </button>
                 <a href='contact' 
-                className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>
+                className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo
+                dark:border-white/50'>
                     Contact 
-                    <Image src={assets.arrow_icon} alt="" className='w-3' /></a>
+                    <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt="" className='w-3' /></a>
 
                     <button className='block md:hidden ml-3' onClick={openMenu}>
-                    <Image src={assets.menu_black} alt='' className='w-6'/> 
+                    <Image src={isDarkMode ? assets.menu_white : assets.menu_black} alt='' className='w-6'/> 
                 </button>
             </div>
 
             {/* -------mobile menu-------- */}
 
             <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64
-             top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 '>
+             top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>
 
                 <div className='absolute right-6 top-6' onClick={closeMenu}>
-                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+                    <Image src={setIsDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer'/>
                 </div>
                 <li><a className='font-Ovo' onClick={closeMenu} href='#top'>Home</a></li>
                 <li><a className='font-Ovo' onClick={closeMenu} href='#about'>About Me</a></li>
