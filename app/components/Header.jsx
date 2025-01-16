8b1d37c20ba6c988c55300eee25968a6906a24f7
Massika-Ke <wamwoka99@gmail.com>
2025-01-16 22:56:35 +0300

    Initial Commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -14,9 +14,13 @@ const Header = () => {
         >
             <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
         </motion.div>
-        <h3 className='flex items-end gap-2 text-xl  md:text-2xl mb-3 font-Ovo'>
+        <motion.h3
+        initial={{y: -20, opacity: 0}}
+        whileInView={{y: 0, opacity: 1}}
+        transition={{duration: 0.6, delay: 0.3}}
+         className='flex items-end gap-2 text-xl  md:text-2xl mb-3 font-Ovo'>
             Hi! I'm Joseph Masika <Image src={assets.hand_icon} alt='' className='w-6'/>
-        </h3>
+        </motion.h3>
         <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
             Software and Network Developer.
         </h1>
