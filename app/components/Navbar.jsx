e898736d200f84c5b8f5418fe269e4799f136f57
Massika-Ke <wamwoka99@gmail.com>
2025-03-08 02:44:09 +0300

    fixed -bug

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
                 <button onClick={()=> setIsDarkMode(prev => !prev)}>
                     <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6'/> 
                 </button>
-                <a href='contact' 
+                <a href='#contact' 
                 className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo
                 dark:border-white/50'>
                     Contact 
