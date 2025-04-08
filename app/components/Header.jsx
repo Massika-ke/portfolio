87436a7cf78dcb4efce3e6827aff4e1f5604822d
Massika-Ke <wamwoka99@gmail.com>
2025-04-08 13:00:51 +0300

    Initial Commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
         whileInView={{opacity: 1}}
         transition={{duration: 0.6, delay: 0.7}}
         className='max-w-2xl mx-auto font-Ovo'>
-            I am a software engineer specializing in building and designing exceptional digital experiences and deploying network architectures.
+            I am a software engineer specializing in building and designing exceptional digital experiences and deploying network operation architectures.
         </motion.p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
             <motion.a 
